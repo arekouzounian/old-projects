6a76f1100e1fec04cd5f7df230ac783acba12fd5
Arek Ouzounian <agouzo777@gmail.com>
2024-03-15 19:16:31 -0700

    added hackathons folder

---
+++ hackathons/README.md
@@ -0,0 +1,2 @@
+# Hackathons
+This folder will store the files for past hackathons that I worked on in high school
---
+++ hackathons/hackademia/alarmclocktest/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace alarmclocktest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DescText = new System.Windows.Forms.Label();
+            this.performanceCounter1 = new System.Diagnostics.PerformanceCounter();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DescText
+            // 
+            this.DescText.AutoSize = true;
+            this.DescText.Location = new System.Drawing.Point(337, 31);
+            this.DescText.Name = "DescText";
+            this.DescText.Size = new System.Drawing.Size(128, 17);
+            this.DescText.TabIndex = 0;
+            this.DescText.Text = "Motion Alarm Clock";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(810, 501);
+            this.Controls.Add(this.DescText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label DescText;
+        private System.Diagnostics.PerformanceCounter performanceCounter1;
+    }
+}
+
